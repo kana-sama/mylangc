127a9f7252682778b37abb48c62ba21a1574574f
kana <andrew4chrome@gmail.com>
2022-01-28 16:08:57 +0300

    generalize tests

--- tests/Language/MyLang/StackMachineSpec.hs
+++ tests/Language/MyLang/StackMachineSpec.hs
@@ -1,38 +1,7 @@
 module Language.MyLang.StackMachineSpec where
 
-import Data.Functor ((<&>))
-import Language.MyLang.Parser (parseFile)
 import Language.MyLang.StackMachine (compute)
-import System.FilePath ((<.>), (</>))
-import System.FilePattern.Directory (getDirectoryFiles)
-import Test.Tasty
-import Test.Tasty.HUnit
+import Language.MyLang.TestData (mkSpec)
 
-spec :: IO TestTree
-spec = do
-  regression <- dirTests "regression"
-  -- expressions <- dirTests "expressions"
-  -- deep_expressions <- dirTests "deep-expressions"
-  pure (testGroup "[SM] tests-data/regression" [regression])
-
-dirTests :: FilePath -> IO TestTree
-dirTests dir = do
-  tests <- getTests ("tests-data" </> dir)
-  let cases =
-        tests <&> \(sourcePath, inputPath, outputPath) -> testCase ("[SM] " <> sourcePath) do
-          source <- parseFile sourcePath
-          input <- parseInput inputPath
-          output <- readFile outputPath
-          result <- compute source input
-          result @?= output
-  pure (testGroup ("[SM] " <> dir) cases)
-
-getTests :: FilePath -> IO [(FilePath, FilePath, FilePath)]
-getTests dir = do
-  sources <- getDirectoryFiles dir ["*.mylang"]
-  pure . take 12 $ [(dir </> s, dir </> s <.> "input", dir </> s <.> "output") | s <- sources]
-
-parseInput :: FilePath -> IO [Int]
-parseInput path = do
-  src <- readFile path
-  pure [read l | l <- lines src]
+spec = mkSpec "SM" \stm input -> do
+  compute stm [read l | l <- lines input]
---
+++ tests/Language/MyLang/TestData.hs
@@ -0,0 +1,40 @@
+module Language.MyLang.TestData (mkSpec) where
+
+import Data.Functor ((<&>))
+import Language.MyLang.AST (Stm)
+import Language.MyLang.Desugarer (desugar)
+import Language.MyLang.Parser (parseFile)
+import System.FilePath ((<.>), (</>))
+import System.FilePattern.Directory (getDirectoryFiles)
+import Test.Tasty
+import Test.Tasty.HUnit
+
+type Input = String
+
+type Output = String
+
+type Runner = (Stm -> Input -> IO Output)
+
+mkSpec :: String -> Runner -> IO TestTree
+mkSpec name run = do
+  regression <- dirTests "regression" name run
+  -- expressions <- dirTests "expressions" name run
+  -- deep_expressions <- dirTests "deep-expressions" name run
+  pure (testGroup ("[" <> name <> "] tests-data/regression") [regression])
+
+dirTests :: FilePath -> String -> Runner -> IO TestTree
+dirTests dir name run = do
+  tests <- getTests ("tests-data" </> dir)
+  let cases =
+        tests <&> \(sourcePath, inputPath, outputPath) -> testCase ("[" <> name <> "] " <> sourcePath) do
+          stm <- desugar <$> parseFile sourcePath
+          input <- readFile inputPath
+          output <- readFile outputPath
+          result <- run stm input
+          result @?= output
+  pure (testGroup ("[" <> name <> "] " <> dir) cases)
+
+getTests :: FilePath -> IO [(FilePath, FilePath, FilePath)]
+getTests dir = do
+  sources <- getDirectoryFiles dir ["*.mylang"]
+  pure . take 12 $ [(dir </> s, dir </> s <.> "input", dir </> s <.> "output") | s <- ("test016.mylang" : sources)]
